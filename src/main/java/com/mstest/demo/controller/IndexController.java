2093c295dc8176b5f171a41ea291c44a1e3a6d90
xiaoliangliang123 <40132876+xiaoliangliang123@users.noreply.github.com>
2020-03-26 09:12:37 +0800

    Update IndexController.java
    update

--- src/main/java/com/mstest/demo/controller/IndexController.java
+++ src/main/java/com/mstest/demo/controller/IndexController.java
@@ -3,7 +3,7 @@ package com.mstest.demo.controller;
 /**
  * @author:WANGLIANG(XIAO)
  * @Date: 2020/3/26 09:04
- * @Description :
+ * @Description :index controller
  */
 public class IndexController {
 }
