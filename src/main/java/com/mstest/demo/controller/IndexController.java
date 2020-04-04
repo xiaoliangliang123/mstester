8bb1b56880c540b0c35f89d65d69261d1827297d
xiaoliangliang123 <40132876+xiaoliangliang123@users.noreply.github.com>
2020-04-04 19:59:07 +0800

    test changed
    test changed

--- src/main/java/com/mstest/demo/controller/IndexController.java
+++ src/main/java/com/mstest/demo/controller/IndexController.java
@@ -3,7 +3,7 @@ package com.mstest.demo.controller;
 /**
  * @author:WANGLIANG(XIAO)
  * @Date: 2020/3/26 09:04
- * @Description :index test controller
+ * @Description :index test change controller
  */
 public class IndexController {
 }
