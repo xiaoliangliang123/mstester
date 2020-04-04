aeeff4622b606f734200773e5ffadd2551a12257
wangliang <1437033605@qq.com>
2020-04-04 19:41:46 +0800

    test change msg

--- src/main/java/com/mstest/demo/controller/IndexController.java
+++ src/main/java/com/mstest/demo/controller/IndexController.java
@@ -1,9 +1,22 @@
 package com.mstest.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
 /**
  * @author:WANGLIANG(XIAO)
  * @Date: 2020/3/26 09:04
  * @Description :index controller
  */
+@Controller
 public class IndexController {
+
+
+    @RequestMapping("/index")
+    public void index(){
+        System.out.println("index");
+    }
+
 }
